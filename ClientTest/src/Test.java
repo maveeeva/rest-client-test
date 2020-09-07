8aee364fc962840d49be936385f8dc4dfa27eea6
maveeeva <70906612+maveeeva@users.noreply.github.com>
2020-09-07 11:30:40 -0400

    Add files via upload

---
+++ ClientTest/src/Test.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+public class Test {
+	
+	 public static void main(String[] args) throws ClientProtocolException, IOException {
+		  HttpClient client = new DefaultHttpClient();
+		  HttpGet request = new HttpGet("http://192.168.1.165:8083/student/allstudent");
+		  HttpResponse response = client.execute(request);
+		  BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+		  String line = "";
+		  while ((line = rd.readLine()) != null) {
+		    System.out.println(line);
+		  }
+		 }
+
+}
